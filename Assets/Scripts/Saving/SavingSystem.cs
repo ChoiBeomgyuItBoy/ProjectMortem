22ea3642b3c77590bbaf1c2744f9bcb8620b6f1c
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2022-12-18 00:47:59 -0600

    Updated Saving System to save and load player's position, using Unicode for Serializing & Deserializing Vector3s w. BitConverter

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.IO;
+using System;
 
 namespace Mortem.Saving
 {
@@ -11,43 +12,68 @@ namespace Mortem.Saving
         {
             string path = GetPathFromSaveFile(saveFile);
             print($"Saving to {path}");
-            WriteHolaMundoToFile(path);
-        }
-
-        private void WriteHolaMundoToFile(string path)
-        {
-            FileStream stream = File.Open(path, FileMode.Create);
-           
-            stream.WriteByte(0xc2); stream.WriteByte(0xa1); // ¡
-
-            stream.WriteByte(0x48); // H
-
-            stream.WriteByte(0x6f); // o
-
-            stream.WriteByte(0x6c); // l
-
-            stream.WriteByte(0x61); // a
-
-            stream.WriteByte(0x20); //
-
-            stream.WriteByte(0x4d); // M
-
-            stream.WriteByte(0x75); // u
-
-            stream.WriteByte(0x6e);  // n
-
-            stream.WriteByte(0x64); // d
-
-            stream.WriteByte(0x6f); // o
-
-            stream.WriteByte(0x21); // !
-
-            stream.Close();
+            WriteDataToPath(path);
         }
 
         public void Load(string saveFile)
         {
+            string path = GetPathFromSaveFile(saveFile);
             print($"Loading from {GetPathFromSaveFile(saveFile)}");
+            ReadDataFromPath(path);
+        }
+
+        private void WriteDataToPath(string path)
+        {
+            using(FileStream stream = File.Open(path, FileMode.Create))
+            {
+                Transform playerTransform = GetPlayerTransform();
+
+                byte[] data = SerializeVector(playerTransform.position);
+
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        private void ReadDataFromPath(string path)
+        {
+            using(FileStream stream = File.Open(path, FileMode.Open))
+            {
+                Transform playerTransform = GetPlayerTransform();
+
+                byte[] dataBuffer = new byte[stream.Length];
+                stream.Read(dataBuffer, 0, dataBuffer.Length);
+
+                playerTransform.GetComponent<CharacterController>().enabled = false;
+                playerTransform.position = DeserializeVector(dataBuffer);
+                playerTransform.GetComponent<CharacterController>().enabled = true;
+            }
+        }
+
+        private byte[] SerializeVector(Vector3 vector)
+        {
+            byte[] vectorBytes = new byte[3 * sizeof(float)];
+
+            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
+
+            return vectorBytes;
+        }
+
+        private Vector3 DeserializeVector(byte[] dataBuffer)
+        {
+            Vector3 vector = new Vector3();
+
+            vector.x =  BitConverter.ToSingle(dataBuffer, 0);
+            vector.y =  BitConverter.ToSingle(dataBuffer, 4);
+            vector.z =  BitConverter.ToSingle(dataBuffer, 8);
+
+            return vector;
+        }
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
         }
 
         private string GetPathFromSaveFile(string saveFile)
